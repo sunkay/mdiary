028c5a8e71f78a1bc23fd9840688f4695ad156fc
sunkay <sunkay11@gmail.com>
2015-10-11 18:25:30 -0400

    responsive slidenav implementation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,6 +5,9 @@ var IndexLink = ReactRouter.IndexLink;
 var Link = ReactRouter.Link;
 
 module.exports = React.createClass({
+  componentDidMount: function() {
+    $(".button-collapse").sideNav();
+  },
   render: function(){
     var now = new Date();
     now.setHours(now.getHours() + 4);
@@ -13,13 +16,24 @@ module.exports = React.createClass({
         <div className="nav-wrapper">
           <Link id="logo-container" to="/" className="brand-logo"> mDiary </Link>
           <ul className="right hide-on-med-and-down">
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/privacy">Privacy</Link></li>
+            <li><Link to="/terms">Terms</Link></li>
             <li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/privacy">Privacy</Link></li>
-              <li><Link to="/terms">Terms</Link></li>
               <p><Time value={now} format="MM.DD HH:mm" /></p>
             </li>
           </ul>
+
+          {/* Responsive hamburger menu on small screens */}
+          <a href="#" data-activates="slide-out" className="button-collapse" >
+            <i className="mdi-navigation-menu"></i>
+          </a>
+          <ul id="slide-out" className="side-nav">
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/privacy">Privacy</Link></li>
+            <li><Link to="/terms">Terms</Link></li>
+          </ul>
+
         </div>
       </nav>
     );
